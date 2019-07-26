497a7288d98c8214878f1d78c0068efb603707c4
colton lacey <coltnutmiles14@gmail.com>
2019-07-26 09:40:06 -0700

    update carpooldata cs

--- Context/CarPoolData.cs
+++ Context/CarPoolData.cs
@@ -1,31 +1,31 @@
 using System;
 using System.Collections.Generic;
-using mathletics.Context;
+using carPool.Context;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-// using mathletics.Context;
+// using carPool.Context;
 
-namespace mathletics
+namespace carPool
 {
-    public static class MathleticsData
+    public static class CarPoolData
     {
-        public static void CreateQuiz(List<Question> quiz)
+        public static void CreateQuiz(List<Profile> quiz)
         {
-            using (var db = new MathleticsContext())
+            using (var db = new CarPoolContext())
             {                   
-                db.Database.ExecuteSqlCommand("TRUNCATE TABLE Question");
-                foreach(Question question in quiz) {
-                    db.Question.Add(question);
+                db.Database.ExecuteSqlCommand("TRUNCATE TABLE Profile");
+                foreach(Profile profilee in quiz) {
+                    db.Profile.Add(profile);
                 }
                 db.SaveChanges();
             }
         }
 
-        public static List<Question> GetQuiz()
+        public static List<Profile> GetQuiz()
         {
-            using (var db = new MathleticsContext())
+            using (var db = new CarPoolContext())
             {
-                return db.Question.ToList();
+                return db.Profile.ToList();
             }
         }
     }
