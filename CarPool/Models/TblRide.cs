c6a86ed33a15fd7ead297dfca4571a52541e8481
colton lacey <coltnutmiles14@gmail.com>
2019-07-31 10:25:32 -0700

    create table contructors

---
+++ CarPool/Models/TblRide.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarPool.Models
+{
+    public partial class TblRide
+    {
+        public int RideId { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string UserName { get; set; }
+    }
+}
